2095edf8840af70dc1fac70995a48c4bb6e89967
Meghana KR <meghanakr2004@gmail.com>
2026-03-30 21:08:31 +0530

    CORS

--- edureach-platform/server/src/app.ts
+++ edureach-platform/server/src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 app.use(
   cors({
-    origin: "edu-reach-agentic-project.vercel.app",
+    origin: "https://edu-reach-agentic-project.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
